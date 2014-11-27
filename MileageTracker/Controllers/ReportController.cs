a717a2e45d9568cc531fb20032857a49681ffc6e
izmaxx <isunhachawi@gmail.com>
2014-11-26 23:04:34 -0500

    Added report with parameter of employeeId

---
+++ MileageTracker/Controllers/ReportController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MileageTracker.Controllers
+{
+    [Authorize]
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index(string employeeId = null)
+        {
+            
+            if (employeeId != null)
+            {
+                string reportServerUrl = ConfigurationManager.AppSettings["ReportServerUrl"];
+                string reportUrl = reportServerUrl + "&employeeId=" + employeeId;
+
+                return Redirect(reportUrl);
+                //ViewBag.ReportUrl = reportUrl;
+            }
+            
+           // string userName = System.Web.Configuration.WebConfigurationManager.AppSettings["ApiUserName"];
+
+            return View();
+        }
+    }
+}
